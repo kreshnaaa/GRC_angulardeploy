a79b77fb30909e4f9f4017114d85f064193d9133
kreshnaaa <102576385+kreshnaaa@users.noreply.github.com>
2024-04-16 15:00:30 +0530

    Update build_and-deploy.ps1

--- scripts/build_and-deploy.ps1
+++ scripts/build_and-deploy.ps1
@@ -1,8 +1,26 @@
-# Install dependencies
-npm install
+jobs:
+  deploy:
+    runs-on: self-hosted
 
-# Build the website
-npm run build
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v2
 
-# Deploy to GitHub Pages
-ghp-import -n -p -f build
+    - name: Setup Node.js
+      uses: actions/setup-node@v2
+      with:
+        node-version: '18.14'
+
+    - name: Install dependencies
+      run: npm install # Or yarn install if you're using yarn
+
+    - name: Build website
+      run: npm run build # Or whatever command is required to build your website
+
+    - name: Deploy to GitHub Pages
+      uses: peaceiris/actions-gh-pages@v3
+      with:
+        github_token: ${{ secrets.ACCESS_TOKEN }}
+        publish_dir: ./build # Update this to the directory containing your built website
+        publish_branch: gh-pages
+
