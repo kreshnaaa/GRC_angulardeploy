3b059aa636fc68f1e7a04c3cd44bfd3883145fcf
kreshnaaa <102576385+kreshnaaa@users.noreply.github.com>
2024-04-17 10:53:05 +0530

    Update build_and-deploy.ps1

--- scripts/build_and-deploy.ps1
+++ scripts/build_and-deploy.ps1
@@ -1,26 +1,7 @@
-jobs:
-  deploy:
-    runs-on: self-hosted
+# This script assumes Node.js and npm are already installed and configured.
 
-    steps:
-    - name: Checkout repository
-      uses: actions/checkout@v2
+# Install dependencies
+npm install
 
-    - name: Setup Node.js
-      uses: actions/setup-node@v2
-      with:
-        node-version: '18.14'
-
-    - name: Install dependencies
-      run: npm install # Or yarn install if you're using yarn
-
-    - name: Build website
-      run: npm run build # Or whatever command is required to build your website
-
-    - name: Deploy to GitHub Pages
-      uses: peaceiris/actions-gh-pages@v3
-      with:
-        github_token: ${{ secrets.ACCESS_TOKEN }}
-        publish_dir: ./build # Update this to the directory containing your built website
-        publish_branch: gh-pages
-
+# Build the website
+npm run build
